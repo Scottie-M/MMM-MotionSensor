f182007b06ccaeb4bf635c9a92630c603ed138db
Scott Moncrieff <scottcmoncrieff@gmail.com>
2025-11-01 11:18:45 +0000

    Create node_helper.js with module helper functions

---
+++ node_helper.js
@@ -0,0 +1,36 @@
+var NodeHelper = require("node_helper");
+
+// add require of other javascripot components here
+// var xxx = require('yyy') here
+
+module.exports = NodeHelper.create({
+
+	init(){
+		console.log("init module helper SampleModule");
+	},
+
+	start() {
+		console.log('Starting module helper:' +this.name);
+	},
+
+	stop(){
+		console.log('Stopping module helper: ' +this.name);
+	},
+
+	// handle messages from our module// each notification indicates a different messages
+	// payload is a data structure that is different per message.. up to you to design this
+	socketNotificationReceived(notification, payload) {
+		console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		// if config message from module
+		if (notification === "CONFIG") {
+			// save payload config info
+			this.config=payload
+			// wait 15 seconds, send a message back to module
+			setTimeout(()=> { this.sendSocketNotification("message_from_helper"," this is a test_message")}, 15000)
+		}
+		else if(notification === "????2") {
+		}
+
+	},
+
+});
